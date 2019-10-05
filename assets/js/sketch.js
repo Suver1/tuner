c279744f1ec56e8a2c76b35755189f73a7e5aeb6
Andreas Hellesvik <andreashellesvik@hotmail.com>
2019-10-05 15:37:18 +0000

    Load ml5 model crepe. Listen to frequency from microphone.

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,8 +1,46 @@
-function setup() {
-    createCanvas(400, 400);
+const modelUrl = 'https://crepe.ahoi.no' // source: https://github.com/ml5js/ml5-data-and-models/tree/master/models/pitch-detection/crepe
 
+let audioContext
+let mic
+let pitch
+let startBtn;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    startBtn = createButton('Start tuning')
+    startBtn.position(width/2-startBtn.width/2, height/2-startBtn.height/2)
+    startBtn.mousePressed(startListening)
+}
+
+function listening() {
+    pitch = ml5.pitchDetection(
+        modelUrl,
+        audioContext,
+        mic.stream,
+        modelLoaded
+    )
+}
+
+function modelLoaded() {
+    pitch.getPitch(getPitchCallback)
+}
+
+function getPitchCallback(error, frequency) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(frequency)
+    }
+    pitch.getPitch(getPitchCallback)
+}
+
+function startListening() {
+    startBtn.remove()
+    audioContext = new AudioContext()
+    mic = new p5.AudioIn()
+    mic.start(listening)
 }
 
 function draw() {
-    background(240);
+    background(240)
 }
